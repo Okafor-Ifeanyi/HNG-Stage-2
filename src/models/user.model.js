07c31404ca08f6a873ba7f403e34fe780bdcf86d
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2023-09-12 08:20:11 +0100

    Feat: Added Model and Error Handler to handle route

---
+++ src/models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    age: {
+        type: Number,
+        trim: true,
+        required: true,
+    },
+    phone_number: {
+        type: String, 
+        trim: true
+    }
+}, { timestamps: true });
+
+const userModel = mongoose.model("User", userSchema)
+
+module.exports = userModel
